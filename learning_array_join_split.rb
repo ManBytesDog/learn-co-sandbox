4afe76cd03f1ae4ca638b68daa6cb09e8748bbeb
T.J. Marstiller <thomasj.marstiller@yahoo.com>
2020-05-24 16:02:38 +0000

    Done.

--- learning_array_join_split.rb
+++ learning_array_join_split.rb
@@ -6,3 +6,16 @@ puts ["This", "is", "a", "test"].join('*')
 message = ["I", "love", "Ruby"]
 puts message.join('_')
 
+# Array of words literal operator 
+
+%w[this is also a test]
+#=> ["this", "is", "also", "a", "test"]
+
+# For method-chaining fun:
+%w[this is also a test].join(" ").capitalize 
+#=> "This is also a test"
+
+#Split a string into an array of words with .split 
+
+"When in the course of human events".split(" ")
+#=> ["When", "in", "the", "course", "of", "human", "events"]
---
+++ learning_ternary_expression.rb
@@ -0,0 +1,15 @@
+# Position 1                # Position 2                # Position 3
+#boolean_conditional_value ? "thing_to_return_if_true" : "thing_to_return_if_false"
+
+#example
+
+likely_to_rain = true 
+garment = likely_to_rain ? "galoshes" : "sun hat"
+
+puts garment
+
+#or another way...
+
+what_to_wear = 2 < 1 ? "galoshes" : "sun hat"
+
+puts what_to_wear
